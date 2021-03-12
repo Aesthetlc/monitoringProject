1daa9558cfce702bbd8e5022dacb9b0603bfcc0b
yangronghui <35282196+Aesthetlc@users.noreply.github.com>
2021-03-12 15:45:10 +0800

    删除测试json

--- src/api/index.js
+++ src/api/index.js
@@ -41,12 +41,3 @@ export function getAlertTrendingTop(params) {
         params
     });
 }
-
-//获取实时监控
-export function fetchData(params) {
-    return request({
-        url: './table.json',
-        method: 'get',
-        params
-    });
-}
